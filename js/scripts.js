5ca94c73f7e83d969d47a49ef978bd090463b493
Derick Hansraj <derickhansraj@ymail.com>
2021-09-11 13:06:32 -0400

    Week 4 assignments

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,22 @@
-$(function() {
-    $(".carousel").carousel( { interval: 2000, pause: "false" } );
-      $("#carouselButton").click(function(){
-          if ($("#carouselButton").children("i").hasClass("fa-pause")) {
-              $(".carousel").carousel("pause");
-              $("#carouselButton").children("i").removeClass("fa-pause");
-              $("#carouselButton").children("i").addClass("fa-play");
-          } else {
-              $(".carousel").carousel("cycle");
-              $("#carouselButton").children("i").removeClass("fa-play");
-              $("#carouselButton").children("i").addClass("fa-pause"); 
-          }
-      });
+$(function () {
+  $(".carousel").carousel({ interval: 2000, pause: "false" });
+  $("#carouselButton").click(function () {
+    if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+      $(".carousel").carousel("pause");
+      $("#carouselButton").children("i").removeClass("fa-pause");
+      $("#carouselButton").children("i").addClass("fa-play");
+    } else {
+      $(".carousel").carousel("cycle");
+      $("#carouselButton").children("i").removeClass("fa-play");
+      $("#carouselButton").children("i").addClass("fa-pause");
+    }
   });
+
+  $("#loginButton").click(function () {
+    $("#loginModal").modal("show");
+  });
+
+  $("#reserveButton").click(function () {
+    $("#reserveModal").modal("show");
+  });
+});
